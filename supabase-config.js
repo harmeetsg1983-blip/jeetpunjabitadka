223c90ab5bf6b5468738674626ea2988d5880db2
harmeetsg1983-blip <harmeetsg1983@gmail.com>
2026-08-27 23:55:48 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Supabase public browser configuration.
+window.JPT_SUPABASE_URL = 'https://qrkbhrmxejtpvheplath.supabase.co';
+window.JPT_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_xySAr0cGnVCaC_sgtwk8Zw_IW_b5sgh';
