178fe3a33adecf1f88aff12496747ef029d7bac5
harmeetsg1983-blip <harmeetsg1983@gmail.com>
2026-09-25 12:00:34 +0530

    Add customer app service worker

---
+++ customer-app-sw.js
@@ -0,0 +1,8 @@
+const CACHE="jpt-customer-shell-v1";
+const ASSETS=["./customer-app.html","./customer-manifest.webmanifest","./index.html","./jpt-customer-icon-192.png","./jpt-customer-icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match("./customer-app.html"))));
+});
