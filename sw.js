684adcfe739875145accd43cbd2269f60b22aa9f
harmeetsg1983-blip <harmeetsg1983@gmail.com>
2026-09-25 12:47:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jpt-ordering-v2";
+const CACHE="jpt-ordering-v3";
 const ASSETS=["./","./index.html","./Jeet_Punjabi_Tadka_Smart_Menu_FINAL.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
